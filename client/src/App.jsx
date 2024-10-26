cf4baf8b54ea621cce4c2192bc568f35f5d4d069
Nguyen Hoang <ntchoang271@gmail.com>
2024-10-26 23:19:09 +0700

    remove redux, use later if neccessary

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,9 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello worlddd!</h1>
+      <h1 className="text-3xl font-bold border-y-blue-600 border-4 underline">Hello worlddd!</h1>
     </>
   );
 }
