4d5bab6d3fbc61385ea54e55a2a00046f2cfc8c8
MinhPhuoc3504 <124911496+MinhPhuoc3504@users.noreply.github.com>
2024-11-13 18:41:24 +0700

    Thêm menu cơ bản

--- client/src/pages/HomePage/HomePage.jsx
+++ client/src/pages/HomePage/HomePage.jsx
@@ -1,12 +1,9 @@
+import Navbar from "@/layouts/Navbar";
+
 function HomePage() {
   return (
     <div>
-      <h1
-        className="bg-blue-800 text-center text-5xl text-white"
-        style={{ padding: "50px" }}
-      >
-        HomePage
-      </h1>
+      <Navbar/>
     </div>
   );
 }
