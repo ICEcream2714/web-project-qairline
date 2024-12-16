6dc5947ec73e0c39a2ea73ff457628c13aaddd3f
MinhPhuoc3504 <124911496+MinhPhuoc3504@users.noreply.github.com>
2024-12-16 17:12:14 +0700

    merge

---
+++ client/src/main_REMOTE_1453.jsx
@@ -0,0 +1,33 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './index.css';
+import HomePage from './pages/HomePage/HomePage';
+import LoginPage from './pages/LoginPage/LoginPage';
+import SignupPage from './pages/SignupPage/SignupPage';
+import ProfilePage from './pages/ProfilePage/ProfilePage';
+import BookingPage from './pages/BookingPage/BookingPage';
+import PassengerDetailsPage from './pages/BookingPage/PassengerDetails';
+import PaymentPage from './pages/BookingPage/Payment';
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/booking" element={<BookingPage />} />
+        <Route
+          path="/booking/passenger-details"
+          element={<PassengerDetailsPage />}
+        />
+        <Route path="/booking/payment" element={<PaymentPage />} />
+
+        {/* Add more routes here */}
+        <Route path="*" element={<h1 className="">404 Not Found</h1>} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
