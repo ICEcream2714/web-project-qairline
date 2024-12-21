8aa980e0982fa28f570a09d86ebd238d7d46f2cb
MinhPhuoc3504 <124911496+MinhPhuoc3504@users.noreply.github.com>
2024-12-21 12:50:42 +0700

    fix Navbar

--- client/src/pages/HomePage/HomePage.jsx
+++ client/src/pages/HomePage/HomePage.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import Booking from '@/pages/HomePage/Booking';
 import PlanningCard from '@/pages/HomePage/PlanningCard';
 import SuggestionHome from '@/pages/HomePage/SuggestionHome';
@@ -8,9 +9,37 @@ import FlightTabs from '@/pages/HomePage/BookingTab';
 import Navbar from '@/layouts/Navbar/Navbar';
 
 function HomePage() {
+  const [isVisible, setIsVisible] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  const handleScroll = () => {
+    if (typeof window !== "undefined") {
+      const currentScrollY = window.scrollY;
+
+      // If the current scroll position is greater than the last scroll position, hide the Navbar
+      if (currentScrollY > lastScrollY) {
+        setIsVisible(false);
+      } else {
+        setIsVisible(true);
+      }
+
+      setLastScrollY(currentScrollY);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [lastScrollY]);
+
   return (
     <div className="-mt-11">
-      <Navbar />
+      {/* Conditionally render Navbar based on scroll direction */}
+      {isVisible && <Navbar />}
+
       <div className="relative">
         {/* Booking Section */}
         <Booking />
